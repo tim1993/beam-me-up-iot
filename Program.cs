f4fbe3d1edb58512ed557f426ce3760eed7c76b3
Dominik Deschner <deschner@medialesson.de>
2022-06-21 20:03:45 +0200

    Adds event loop

--- Program.cs
+++ Program.cs
@@ -1,7 +1,22 @@
-﻿Console.WriteLine("Vibration monitoring starting up!");
+﻿using System.Device.Spi;
+using System.Numerics;
+using System.Text;
+using System.Text.Json;
+using Iot.Device.Adxl345;
+
+Console.WriteLine("Vibration monitoring starting up!");
 
 var deviceClient = await CreateDeviceClient();
+var sensor = InitializeSensor();
 
+
+while (true)
+{
+    var message = CreateTelemetryFromReading(sensor.Acceleration);
+    await deviceClient.SendEventAsync(message);
+
+    await Task.Delay(TimeSpan.FromSeconds(10));
+}
 
 async Task<DeviceClient> CreateDeviceClient()
 {
@@ -12,3 +27,20 @@ async Task<DeviceClient> CreateDeviceClient()
     return deviceClient;
 }
 
+Adxl345 InitializeSensor()
+{
+    var spiBinding = SpiDevice.Create(new SpiConnectionSettings(0, 0)
+    {
+        ClockFrequency = Adxl345.SpiClockFrequency,
+        Mode = Adxl345.SpiMode
+    });
+    var sensor = new Adxl345(spiBinding, GravityRange.Range08);
+
+    return sensor;
+}
+
+Message CreateTelemetryFromReading(Vector3 acceleration)
+{
+    var payload = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(acceleration));
+    return new Message(payload);
+}
